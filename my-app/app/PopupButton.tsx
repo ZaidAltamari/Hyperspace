935a3f8fd9171ac388504a8b82d2ff73725f3104
Zaid mohamad <93926834+ZaidAltamari@users.noreply.github.com>
2024-06-03 13:03:26 +0400

    new

--- my-app/app/PopupButton.tsx
+++ my-app/app/PopupButton.tsx
@@ -19,7 +19,7 @@ export default function PopupButton() {
 				Open Popup
 			</button>
 			{isOpen && (
-				<div className='fixed inset-0 flex items-center justify-center z-50'>
+				<div className='fixed inset-0 flex items-center justify-center z-50 backdrop-blur-lg'>
 					<div
 						className='absolute inset-0 bg-black opacity-50'
 						onClick={handleClosePopup}></div>
